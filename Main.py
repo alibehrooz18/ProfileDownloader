3ead91208ff402e8bf95a35d3c55a5ff34ea08ff
Ali Behrooz <ali.behrooz.2002@gmail.com>
2024-02-05 01:34:23 +0330

    part 1 tkinter library

---
+++ Main.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+window = Tk()
+
+# window size
+window.geometry("600x600")
+window.maxsize(1920, 1080)
+window.minsize(200, 200)
+
+# window title
+window.title("Instagram Profile Downloader")
+
+# window label
+label = Label(window, text="Hello World")
+label.pack()
+
+
+# hello command
+def hello():
+    label.config(text=inputs.get())
+    # print("Hello World")
+
+
+# button design
+button = Button(window, text="click here", fg="blue", command=hello)
+button.pack()   # we can use button.place(x = n, y = n)
+
+button = Button(window, text="close", fg="red", command=window.quit)
+button.place(x=30, y=30)
+
+
+# input
+inputs = Entry(window)
+inputs.pack()
+
+
+window.mainloop()
