586994e24f0ad54ece778031b5569ebd5e7b49c0
Ali Behrooz <ali.behrooz.2002@gmail.com>
2024-02-17 23:16:54 +0330

    for some reason this version doesn't work

---
+++ instaldr.py
@@ -0,0 +1,18 @@
+import instaloader
+
+# Create an instance of Instaloader
+L = instaloader.Instaloader()
+
+L.interactive_login("fifa")
+
+# Load a profile using its username
+profile = instaloader.Profile.from_username(L.context, 'fifa')
+
+# Fetch the followers of the profile
+followers = [follower.username for follower in profile.get_followers()]
+
+# Print the list of followers
+print(followers)
+print(profile.followees)
+print(profile.get_profile_pic_url())
+
